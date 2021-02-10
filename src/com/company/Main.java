bf4f03e88d607417ef1246678bf905c85961a333
oxbey <qqaa_fds@ukr.net>
2021-02-10 19:59:24 +0200

    My first commit git lab

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Person a1 = new Person("aaa", 25);
+        Person a2 = new Person(21, "vvvv");
+        System.out.println(a1.age);
+        System.out.println(a1.name);
+        System.out.println(a2.age);
+        System.out.println(a2.name);
+    }
+}
+
+
+
